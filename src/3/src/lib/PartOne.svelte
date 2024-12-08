fa4dfb710bed3feae8eea5da76a9bc11a49e157c
Adrien <adrien.aco99@gmail.com>
2024-12-08 21:56:08 +0100

    setup(3): day 3 setup

---
+++ src/3/src/lib/PartOne.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  const { lists } = $props();
+  let res: number = $state(0);
+</script>
+
+<h2>part 1</h2>
+<div>difference: {res}</div>
---
+++ src/3/src/lib/PartTwo.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  const { lists } = $props();
+  let res: number = $state(0);
+</script>
+
+<h2>part 2</h2>
+<div>difference: {res}</div>
