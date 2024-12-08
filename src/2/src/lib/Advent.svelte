40c3e0c2a709bb70f334bc19a1d32bdc5a77d16f
Adrien <adrien.aco99@gmail.com>
2024-12-08 18:52:50 +0100

    feat(2): template for day project + setup for day 2

---
+++ src/2/src/lib/Advent.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import PartOne from './PartOne.svelte';
+  import PartTwo from './PartTwo.svelte';
+  import { onMount } from 'svelte';
+
+  let text = '';
+
+  onMount(async () => {
+    const response = await fetch('input.txt');
+    if (response.ok) text = await response.text();
+    else console.error("failed to load the file.");
+  });
+</script>
+
+{#if text.length > 0}
+  <PartOne text="{text}" />
+  <PartTwo text="{text}" />
+{:else}
+  <PartOne text="loading..." />
+  <PartTwo text="loading..." />
+{/if}
