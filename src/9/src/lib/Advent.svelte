925f1f6e63694e63058d3bbd03b95bbd43fe7c58
Adrien <adrien.aco99@gmail.com>
2024-12-13 00:34:18 +0100

    feat(9): setup day 9 + add example input file to template

---
+++ src/9/src/lib/Advent.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import PartOne from './PartOne.svelte';
+  import PartTwo from './PartTwo.svelte';
+  import { onMount } from 'svelte';
+
+  let lists: number[] = [];
+
+  onMount(async () => {
+    let text='';
+    const response = await fetch('input.txt');
+    if (response.ok) text = await response.text();
+    else console.error("failed to load the file.");
+  });
+</script>
+
+{#if lists.length > 0}
+  <PartOne lists="{lists}" />
+  <PartTwo lists="{lists}" />
+{:else}
+  <PartOne lists="loading..." />
+  <PartTwo lists="loading..." />
+{/if}
