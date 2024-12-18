8895106512f8dde25613486ba7bbd33aa48008f7
Adrien <adrien.aco99@gmail.com>
2024-12-18 02:11:26 +0100

    feat(15): parsing setup

--- src/15/src/lib/Advent.svelte
+++ src/15/src/lib/Advent.svelte
@@ -1,22 +1,39 @@
 <script lang="ts">
-  import PartOne from './PartOne.svelte';
-  import PartTwo from './PartTwo.svelte';
-  import { onMount } from 'svelte';
+import PartOne from "./PartOne.svelte";
+import PartTwo from "./PartTwo.svelte";
+import { onMount } from "svelte";
 
-  let lists: number[] = [];
+interface Day15 {
+	map: string[][];
+	instructions: [];
+}
+let inputs: Day15 = {
+	map: [],
+	instructions: [],
+};
 
-  onMount(async () => {
-    let text='';
-    const response = await fetch('input.txt');
-    if (response.ok) text = await response.text();
-    else console.error("failed to load the file.");
-  });
+function parse(text) {
+	const split = text.split("\n\n");
+	console.log(split);
+}
+
+onMount(async () => {
+	let text = "";
+	const response = await fetch("input.txt");
+	if (response.ok) text = await response.text();
+	else console.error("failed to load the file.");
+
+	parse(text);
+
+	// biome-ignore lint/correctness/noSelfAssign: <explanation>
+	inputs = inputs; // svelte reactive
+});
 </script>
 
-{#if lists.length > 0}
-  <PartOne lists="{lists}" />
-  <PartTwo lists="{lists}" />
+{#if inputs.map.length > 0}
+  <PartOne inputs="{inputs}" />
+  <PartTwo inputs="{inputs}" />
 {:else}
-  <PartOne lists="loading..." />
-  <PartTwo lists="loading..." />
+  <PartOne inputs="loading..." />
+  <PartTwo inputs="loading..." />
 {/if}
--- src/15/src/lib/PartOne.svelte
+++ src/15/src/lib/PartOne.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  const { lists } = $props();
-  let res: number = $state(0);
+const { inputs } = $props();
+const res: number = $state(0);
 </script>
 
 <h2>part 1</h2>
